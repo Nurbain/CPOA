69a7bc0bcda05b7a5ee37d6b23c2adb66c70f422
nurbain <nathan.urbain@etu.unistra.fr>
2017-11-07 22:35:28 +0100

    vec(feat): Add function declaration
    Still vectorial product to do

---
+++ vectorMatrix/vector.h
@@ -0,0 +1,38 @@
+/**
+ * @author Nathan Urbain
+ * @date 7/11/2017
+ * @file fonctions de vecteur
+ */
+
+#ifndef _VECTOR_H
+#define _VECTOR_H
+
+#include "../InterfaceCSG/Array.h"
+
+template<int D, typename T>
+class Vector : public Array<T,D>{
+
+    void operator=(const Vector& v);
+    void operator==(const Vector& v);
+
+    Vector operator+(const Vector& v);
+    Vector operator+=(const Vector& v);
+
+    Vector operator-(const Vector& v);
+    Vector operator-=(const Vector& v);
+
+    Vector operator*(double f);
+    Vector operator*=(double f);
+
+    Vector operator/(double f);
+    Vector operator/=(double f);
+
+    double prod_scalaire(const Vector& v, const Vector& u);
+
+    //TODO produit vectoriel
+};
+
+
+#include "vector.hpp"
+
+#endif
---
+++ vectorMatrix/vector.hpp
@@ -0,0 +1,68 @@
+/**
+ * @author Nathan Urbain
+ * @date 7/11/2017
+ * @file fonctions de vecteur
+ */
+
+#ifndef _VECTOR_HPP_
+#define _VECTOR_HPP_
+
+#include "vector.h"
+
+template<int D,typename T>
+void operator=(const Vector& v){
+
+}
+
+template<int D,typename T>
+void operator==(const Vector& v){
+
+}
+
+template<int D,typename T>
+Vector operator+(const Vector& v){
+
+}
+
+template<int D,typename T>
+Vector operator+=(const Vector& v){
+
+}
+
+template<int D,typename T>
+Vector operator-(const Vector& v){
+
+}
+
+template<int D,typename T>
+Vector operator-=(const Vector& v){
+
+}
+
+template<int D,typename T>
+Vector operator*(double f){
+
+}
+
+template<int D,typename T>
+Vector operator*=(double f){
+
+}
+
+template<int D,typename T>
+Vector operator/(double f){
+
+}
+
+template<int D,typename T>
+Vector operator/=(double f){
+
+}
+
+double prod_scalaire(const Vector& v, const Vector& u);
+
+
+
+
+
+#define
