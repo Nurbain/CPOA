6ee20d21d8f53fb86e1f13f080ee77c132c25460
nurbain <nathan.urbain@etu.unistra.fr>
2017-11-08 01:31:38 +0100

    vec(test): add first test =

--- vectorMatrix/tests/test_vec.cpp
+++ vectorMatrix/tests/test_vec.cpp
@@ -4,9 +4,17 @@
 int main()
 {
     Vec3d v1 = {1,2,3};
+    Vec3d v2 = v1;
 
     std::cout << "Test Vecteur" << std::endl;
-    std::cout << v1[0] << std::endl;
+    std::cout << v1[0] << v1[1] << v1[2] << std::endl;
+    std::cout << v2[0] << v2[1] << v2[2] << std::endl;
+
+    if(v1==v2)
+        std::cout << "egality" << std::endl;
+    else
+        std::cout << "No" << std::endl;
+
     //std::cout << v1 << std::endl;
 
 	return 0;
