977fd795280380afa8076dd0f0542922fc28c381
nurbain <nathan.urbain@etu.unistra.fr>
2017-11-08 11:48:24 +0100

    vec(feat): Explicit constructor #3

--- vectorMatrix/vector.h
+++ vectorMatrix/vector.h
@@ -45,12 +45,28 @@ class Vector : public Array<T,D>{
 
 
 // Classe Int
-class Vec2i : public Vector<2,int>{};
-class Vec3i : public Vector<3,int>{};
-class Vec4i : public Vector<4,int>{};
+class Vec2i : public Vector<2,int>{
+    public:
+        Vec2i() : Vector<2,int>() {}
+        Vec2i(std::initializer_list<int> list): Vector<2,int>(list){}
+};
+class Vec3i : public Vector<3,int>{
+    public:
+        Vec3i() : Vector<3,int>() {}
+        Vec3i(std::initializer_list<int> list): Vector<3,int>(list){}
+};
+class Vec4i : public Vector<4,int>{
+    public:
+        Vec4i() : Vector<4,int>() {}
+        Vec4i(std::initializer_list<int> list): Vector<4,int>(list){}
+};
 
 // Classe Float
-class Vec2d : public Vector<2,double>{};
+class Vec2d : public Vector<2,double>{
+    public:
+        Vec2d() : Vector<2,double>() {}
+        Vec2d(std::initializer_list<double> list): Vector<2,double>(list){}
+};
 
 class Vec3d : public Vector<3,double>{
     public:
@@ -58,7 +74,11 @@ class Vec3d : public Vector<3,double>{
         Vec3d(std::initializer_list<double> list): Vector<3,double>(list){}
 };
 
-class Vec4d : public Vector<4,double>{};
+class Vec4d : public Vector<4,double>{
+    public:
+        Vec4d() : Vector<4,double>() {}
+        Vec4d(std::initializer_list<double> list): Vector<4,double>(list){}
+};
 
 
 #include "vector.hpp"
