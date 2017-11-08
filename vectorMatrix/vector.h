9887256f7f40efced740b4f6720beb7c073574e3
nurbain <nathan.urbain@etu.unistra.fr>
2017-11-08 18:33:57 +0100

    vec(refactor): use using instead of class for VecDT declaration

--- vectorMatrix/vector.h
+++ vectorMatrix/vector.h
@@ -45,36 +45,16 @@ class Vector : public Array<T,D>{
 
 
 // Classe Int
-class Vec2i : public Vector<2,int>{
-    public:
-        Vec2i() : Vector<2,int>() {}
-        Vec2i(std::initializer_list<int> list): Vector<2,int>(list){}
-};
-class Vec3i : public Vector<3,int>{
-    public:
-        Vec3i() : Vector<3,int>() {}
-        Vec3i(std::initializer_list<int> list): Vector<3,int>(list){}
-};
-class Vec4i : public Vector<4,int>{
-    public:
-        Vec4i() : Vector<4,int>() {}
-        Vec4i(std::initializer_list<int> list): Vector<4,int>(list){}
-};
+using Vec2i = Vector<2,int>;
+using Vec3i = Vector<3,int>;
+using Vec4i = Vector<4,int>;
 
 // Classe Float
-class Vec2d : public Vector<2,double>{
-    public:
-        Vec2d() : Vector<2,double>() {}
-        Vec2d(std::initializer_list<double> list): Vector<2,double>(list){}
-};
-
+using Vec2d = Vector<2,double>;
 using Vec3d = Vector<3,double>;
+using Vec4d = Vector<4,double>;
 
-class Vec4d : public Vector<4,double>{
-    public:
-        Vec4d() : Vector<4,double>() {}
-        Vec4d(std::initializer_list<double> list): Vector<4,double>(list){}
-};
+
 
 
 #include "vector.hpp"
