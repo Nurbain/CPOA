6684c63a750fb9f0dfb94f0e2db1de90b1b38ea4
nurbain <nathan.urbain@etu.unistra.fr>
2017-11-08 01:32:11 +0100

    mat(feat): Set Matrix33d files

---
+++ vectorMatrix/matrix33d.h
@@ -0,0 +1,15 @@
+/**
+ * @author Nathan Urbain
+ * @date 08/11
+ * @file Matrix33d function header
+ */
+
+#ifndef __MATRIX33D_H__
+#define __MATRIX33D_H__
+
+#include "vector.h"
+
+
+#include "matrix33d.hpp"
+
+#endif
---
+++ vectorMatrix/matrix33d.hpp
@@ -0,0 +1,15 @@
+/**
+ * @author Nathan Urbain
+ * @date 08/11
+ * @file Matrix33d function header implements
+ */
+
+#ifndef __MATRIX33D_HPP__
+#define __MATRIX33D_HPP__
+
+#include "matrix33d.h"
+
+
+
+
+#endif
